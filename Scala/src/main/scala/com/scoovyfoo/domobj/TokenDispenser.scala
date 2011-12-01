d1cc51e4cbe68beb4919b757b6c455d9b0977c0a
Anupam Chandra (AC) <anupam@softexcel.com>
2011-12-01 14:23:39 -0500

    Made the class thread safe

---
+++ Scala/src/main/scala/com/scoovyfoo/domobj/TokenDispenser.scala
@@ -0,0 +1,48 @@
+package com.scoovyfoo.domobj
+
+import actors.Actor
+import actors.Actor.self
+
+/**
+ *
+ * User: Anupam Chandra
+ * Date: 11/29/11
+ * Time: 2:12 PM
+ */
+
+class TokenDispenser(startingNumber: Int) {
+  NumberDispenser.start()
+
+  private object NumberDispenser extends Actor {
+    private def numberStream(num: Int): Stream[Int] =
+      num #:: numberStream(num + 1)
+
+    private var nextNumberStream = numberStream(startingNumber)
+
+    def act() {
+      loop {
+        react {
+          case ("DISPENSE", actor : Actor) => {
+            actor ! nextNumberStream.head;
+            nextNumberStream = nextNumberStream.tail
+          }
+          case ("RESET", resetTo: Int) => {
+            nextNumberStream = numberStream(resetTo)
+          }
+        }
+      }
+    }
+  }
+
+  def reset(resetTo: Int) = {
+      NumberDispenser ! ("RESET",resetTo)
+      this
+  }
+  def nextNumber: Int = {
+    val msg = ("DISPENSE",self)
+    NumberDispenser ! msg
+    self.receive {case nextNum:Int => nextNum}
+  }
+
+
+}
---
+++ Scala/src/test/scala/Scala/com/scoovyfoo/domobj/TokenDispenserTest.scala
@@ -0,0 +1,49 @@
+package Scala.com.scoovyfoo.domobj
+
+import org.junit.Test;
+import org.junit.Assert.assertEquals
+import com.scoovyfoo.domobj.TokenDispenser
+import actors.Actor._
+;
+
+/**
+ *
+ * User: Anupam Chandra
+ * Date: 11/29/11
+ * Time: 3:49 PM
+ */
+
+class TokenDispenserTest {
+  @Test
+  def testNextNumber() {
+    val a = new TokenDispenser(1304)
+    assertEquals(1304, a.nextNumber)
+    assertEquals(1305, a.nextNumber)
+    assertEquals(1306, a.nextNumber)
+
+    for (i <- 1307 to 101307)
+      assertEquals(i, a.nextNumber)
+  }
+
+  @Test
+  def testReset() {
+    val a = new TokenDispenser(1304)
+    assertEquals(1, a.reset(1).nextNumber)
+    assertEquals(2, a.nextNumber)
+    assertEquals(3, a.nextNumber)
+  }
+
+  @Test
+  def testMultiThreaded() {
+    val a = new TokenDispenser(100)
+    val actorOne = actor {
+      for (i <- 1 to 100)
+        println("ActorOne => " + a.nextNumber)
+    }
+    val actorTwo = actor {
+      for (i <- 1 to 100)
+        println("ActorTwo => " + a.nextNumber)
+    }
+  }
+
+}
