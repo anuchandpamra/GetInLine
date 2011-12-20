50789b19001502afe5315bb3dd85a891bf0eb6e9
Anupam Chandra (AC) <anupam@softexcel.com>
2011-12-20 09:01:40 -0500

    Blog-Day-5-2.0: Final code for day 5. Along with Copyright message

--- Lift/src/main/scala/bootstrap/liftweb/Boot.scala
+++ Lift/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,14 +1,9 @@
 package bootstrap.liftweb
 
-import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 import _root_.net.liftweb.http._
 import _root_.net.liftweb.http.provider._
 import _root_.net.liftweb.sitemap._
-import _root_.net.liftweb.sitemap.Loc._
-import Helpers._
-import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor}
-import _root_.java.sql.{Connection, DriverManager}
 import _root_.Lift.model._
 
 
@@ -18,31 +13,15 @@ import _root_.Lift.model._
  */
 class Boot {
   def boot {
-    if (!DB.jndiJdbcConnAvailable_?) {
-      val vendor = 
-	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-			     Props.get("db.url") openOr 
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-			     Props.get("db.user"), Props.get("db.password"))
-
-      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
-
-      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
-    }
-
     // where to search snippet
     LiftRules.addToPackages("Lift")
-    Schemifier.schemify(true, Schemifier.infoF _, User)
 
     // Build SiteMap
     def sitemap() = SiteMap(
-      Menu("Home") / "index"  >> User.AddUserMenusAfter, // Simple menu form
-      // Menu with special Link
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")))
+      Menu("Home") / "index"
+    )
 
-    LiftRules.setSiteMapFunc(() => User.sitemapMutator(sitemap()))
-    //LiftRules.setSiteMapFunc(() => sitemap())
+    LiftRules.setSiteMapFunc(() => sitemap())
     /*
      * Show the spinny image when an Ajax call starts
      */
@@ -56,10 +35,6 @@ class Boot {
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
 
     LiftRules.early.append(makeUtf8)
-
-    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
-
-    S.addAround(DB.buildLoanWrapper)
   }
 
   /**
